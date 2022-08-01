d58bffae2323b3f2fe7e96290e51927b02b54e55
Skahrr <1dgonascaaron@gmail.com>
2022-08-01 12:41:48 +0100

    set up environment to test and start working on app.js

---
+++ __tests__/app.test.js
@@ -0,0 +1,11 @@
+const seed = require("../db/seeds/seed.js");
+const app = require("../app.js");
+const request = require("supertest");
+const db = require("../db/connection.js");
+const data = require("../db/data/test-data");
+
+describe('', ()=>{
+    test('testing', ()=>{
+        expect(1).toBe(2);
+    })
+})
