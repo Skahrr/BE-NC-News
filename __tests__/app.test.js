2fe86518965b9c55b09ee16672e23656c4ed18f5
Skahrr <1dgonascaaron@gmail.com>
2022-08-01 15:56:42 +0100

    tests done with error handling for 2 cases

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -18,16 +18,52 @@ describe("GET /api/topics", () => {
       .get("/api/topics")
       .expect(200)
       .then(({ body }) => {
-        const { topics } = body
+        const { topics } = body;
         expect(Array.isArray(topics)).toEqual(true);
         expect(topics).toHaveLength(3);
         topics.forEach((topic) => {
           expect(topic).toEqual(
             expect.objectContaining({
-            slug: expect.any(String),
-            description: expect.any(String),
-          }))
+              slug: expect.any(String),
+              description: expect.any(String),
+            })
+          );
         });
       });
   });
+});
+
+describe("GET /api/articles/:article_id", () => {
+  test("return the requested article", () => {
+    return request(app)
+      .get("/api/articles/1")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.article).toBeInstanceOf(Object);
+        expect(body.article).toEqual(
+          expect.objectContaining({
+            article_id: expect.any(Number),
+            title: expect.any(String),
+            topic: expect.any(String),
+            author: expect.any(String),
+            body: expect.any(String),
+            created_at: expect.any(String),
+            votes: expect.any(Number),
+          })
+        );
+      });
+  });
+  test("should respond with 400 error when passed an invalid id(NaN)", () => {
+    return request(app)
+      .get("/api/articles/uno")
+      .expect(400)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Bad Request!");
+      });
+  });
+  test('should respond with 404 error when passed a valid id but doesnt exist on db', ()=>{
+    return request(app).get('/api/articles/123123').expect(404).then(({body})=>{
+        expect(body.msg).toBe('ID not found')
+    })
+  })
 });
