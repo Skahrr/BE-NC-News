4a66370ab5f396095917e5a47ae8135d7f972fdf
Skahrr <1dgonascaaron@gmail.com>
2022-08-04 09:19:17 +0100

    excluded body from article

--- models/app.model.js
+++ models/app.model.js
@@ -49,7 +49,7 @@ exports.fetchUsers = () => {
 
 exports.fetchArticles = () => {
   return db.query(
-    "SELECT articles.*, CAST(COUNT(comment_id) AS int) AS comment_count FROM articles LEFT JOIN comments ON articles.article_id= comments.article_id GROUP BY articles.article_id ORDER BY created_at"
+    "SELECT articles.article_id, articles.title, articles.topic, articles.author, articles.created_at, articles.votes, CAST(COUNT(comment_id) AS int) AS comment_count FROM articles LEFT JOIN comments ON articles.article_id= comments.article_id GROUP BY articles.article_id ORDER BY created_at"
   ).then(({rows: articles})=>{
     return articles
   })
